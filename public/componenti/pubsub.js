abb5a29240a6e9d6c46eadc5c46bef5e6ebf3b34
Davide Baroni <baronidavide@itis-molinari.eu>
2025-02-26 10:53:44 +0100

    modifiche al pub sub

--- public/componenti/pubsub.js
+++ public/componenti/pubsub.js
@@ -1,13 +1,17 @@
+//gestisce eventi e notifica ad altre parti di codice in asincrono
 export const generatePubSub = () => {
+
     const events = {};
 
     return {
+        
         subscribe: (eventName, callback) => {
             if (!events[eventName]) {
                 events[eventName] = [];
             }
             events[eventName].push(callback);
         },
+
         publish: (eventName, data) => {
             if (events[eventName]) {
                 events[eventName].forEach(callback => callback(data));
